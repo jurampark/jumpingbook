adc4965c3bed1ca0d7ea2d16269079940f992f15
parkjuram <parkjuram@gmail.com>
2015-05-24 16:57:30 +0900

    add login page

--- jumpingbook/main/urls.py
+++ jumpingbook/main/urls.py
@@ -3,4 +3,5 @@ from main.views import IndexView
 
 urlpatterns = patterns('',
    url(r'^$', IndexView.as_view(), name='main'),
+   url(r'^login$', LoginView.as_view(), name='main'),
 )
--- jumpingbook/main/views.py
+++ jumpingbook/main/views.py
@@ -6,3 +6,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "main/index.html"
+
+class LoginView(TemplateView):
+    template_name = "main/login.html"
