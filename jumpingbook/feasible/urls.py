ec31fad4d041806a960142b4609ce461ed809d28
parkjuram <parkjuram@gmail.com>
2015-05-25 19:13:49 +0900

    change main part using template extends

--- jumpingbook/feasible/urls.py
+++ jumpingbook/feasible/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 from feasible import views
-from feasible.views import FacebookView
+from feasible.views import FacebookView, MypageView, ShopView
 
 urlpatterns = patterns('',
    url(r'^facebook$', FacebookView.as_view(), name='facebook'),
+   url(r'^mypage$', MypageView.as_view(), name='mypage'),
+   url(r'^shop$', ShopView.as_view(), name='shop'),
 )
--- jumpingbook/feasible/views.py
+++ jumpingbook/feasible/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class FacebookView(TemplateView):
     template_name = "feasible/facebook.html"
+
+class MypageView(TemplateView):
+    template_name = "feasible/mypage.html"
+
+class ShopView(TemplateView):
+    template_name = "feasible/shop.html"
