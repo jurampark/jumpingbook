4aaebf6975035fc62c4986ca4fea2a81ad7dd99b
parkjuram <parkjuram@gmail.com>
2015-05-25 20:50:08 +0900

    add rated book api

--- jumpingbook/recommend/urls.py
+++ jumpingbook/recommend/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
 from feasible import views
 from feasible.views import FacebookView
-from recommend.views import BookListView, RatingBookView
+from recommend.views import BookListView, RatingBookView, RatedBookListView
 from supervisor.views import ScrapKyoboBookView
 
 urlpatterns = patterns('',
    url(r'^book/$', BookListView.as_view(), name='book_list'),
-   url(r'^book/rating/$', RatingBookView.as_view(), name='book_rating'),
+   url(r'^rating/book/$', RatingBookView.as_view(), name='book_rating'),
+   url(r'^book/rated/$', RatedBookListView.as_view(), name='rated_book'),
+
 )
--- jumpingbook/recommend/views.py
+++ jumpingbook/recommend/views.py
@@ -12,18 +12,35 @@ class BookListView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         already_rating_book_list = request.user.userbookrating_set.values_list('book_id', flat=True)
         books = Book.objects.exclude(id__in=already_rating_book_list).values("id", "name", "author", "publisher", "published_date")
-
         return HttpResponse(json.dumps(list(books), cls=DjangoJSONEncoder), content_type="application/json")
 
 class RatingBookView(CsrfExemptMixin, View):
-
     def post(self, request, *args, **kwargs):
         item_id = request.POST.get('item-id')
-        rating = request.POST.get('rating')
+        rating = int(float(request.POST.get('rating'))*2)
+
 
         try:
-            user_book_rating = UserBookRating.objects.create(user=request.user,book=Book.objects.get(id=item_id),score=rating*2)
+            user_book_rating = UserBookRating.objects.create(user=request.user,book=Book.objects.get(id=item_id),score=rating)
         except Exception as e:
+            print e
             return HttpResponseBadRequest()
 
         return HttpResponse(json.dumps({}, cls=DjangoJSONEncoder), content_type="application/json")
+
+class RatedBookListView(CsrfExemptMixin, LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        user_book_ratings = request.user.userbookrating_set.select_related('book').all()
+        rated_books = []
+        for user_book_rating in user_book_ratings:
+            book = user_book_rating.book
+            rated_books.append({
+                "id": book.id,
+                "name": book.name,
+                "author": book.author,
+                "publisher": book.publisher,
+                "published_date": book.published_date,
+                "score": user_book_rating.score
+            })
+
+        return HttpResponse(json.dumps(rated_books, cls=DjangoJSONEncoder), content_type="application/json")
