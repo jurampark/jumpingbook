4b5f90ecf7a072d831b39c85e5701b60547b3af8
unknown <jjune1416@naver.com>
2015-05-25 18:48:27 +0900

    book rating add

---
+++ jumpingbook/static/js/main/mypage.js
@@ -0,0 +1,65 @@
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+$( document ).ready(function() {
+
+    $('input.rating').rating();
+
+    $('#my-comment').click( function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: urlForBookList,
+            data: {},
+            success: function(data){
+                $('input.rating').off();
+                for(var index in data) {
+                    var item = data[index];
+                    var elem = "    <div class=\"book-item\" item-id=\"" + item['id'] + "\"> \
+                                        <div class=\"img-container\"> \
+                                            <img src=\"http://youth.sangju.go.kr/fileUpload/contentsboard/book_00406.jpg\" /> \
+                                        </div> \
+                                        <div class=\"star-rating-container\"> \
+                                            <div class=\"item-title\">" + item['name'] + "</div> \
+                                            <div class=\"item-author\">" + item['author'] + "</div> \
+                                            <input type=\"hidden\" class=\"rating\" data-fractions=\"2\"/> \
+                                        </div> \
+                                    </div>";
+                    $('.book-item-container').append( elem );
+                }
+                $('input.rating').rating();
+                $('input.rating').on('change', function () {
+                    var current_item = $(this).parent().parent();
+                    $.ajax({
+                        type: 'POST',
+                        url: urlForBookRating,
+                        data: {
+                            "item-id": $(this).parent().parent().attr('item-id'),
+                            "rating" : $(this).val()
+                        },
+                        success:function()
+                        {
+                          current_item.remove();
+                        },
+                        error: function(xhr, type, exception) {
+                            alert("ajax error response type2 "+type);
+                        }
+                    })
+                })
+            },
+            error: function(xhr, type, exception) {
+                alert("ajax error response type1 "+type);
+            }
+        });
+    });
+
+});
