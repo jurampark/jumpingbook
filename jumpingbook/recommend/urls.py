cd9547e7dbb03a38ac22bdce3baa7b751580efe5
parkjuram <parkjuram@gmail.com>
2015-05-27 17:36:39 +0900

    solve problem

--- jumpingbook/recommend/urls.py
+++ jumpingbook/recommend/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, url
-from feasible import views
-from feasible.views import FacebookView
-from recommend.views import BookListView, RatingBookView, RatedBookListView
-from supervisor.views import ScrapKyoboBookView
+from recommend.views import BookListView, RatingBookView, RatedBookListView, CancelRatingBookView
 
 urlpatterns = patterns('',
    url(r'^book/$', BookListView.as_view(), name='book_list'),
    url(r'^rating/book/$', RatingBookView.as_view(), name='book_rating'),
    url(r'^book/rated/$', RatedBookListView.as_view(), name='rated_book'),
    url(r'^rating/book/cancel$', CancelRatingBookView.as_view(), name='cancel_rating'),
-
 )
