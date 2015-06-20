08d54b86998502ae53e5a9b5c667cbe004ae1f5f
parkjuram <parkjuram@gmail.com>
2015-06-20 21:38:15 +0900

    fabfile update

--- jumpingbook/fabfile.py
+++ jumpingbook/fabfile.py
@@ -8,9 +8,10 @@ env.user = 'ec2-user'
 env.key_filename = '~/.ssh/ramju.pem'
 
 git_repo_url = "git@github.com:parkjuram/jumpingbook.git"
-project_home_directory  = '~/workspace/django/jumpingbook'
-project_source_directory = project_home_directory + '/source'
-project_virtualenv_directory = project_home_directory + '/virtualenv'
+remote_root_directory  = '~/workspace/django/jumpingbook'
+remote_git_directory = remote_root_directory + '/source'
+remote_source_directory = remote_root_directory + '/source/jumpingbook'
+remote_virtualenv_directory = remote_root_directory + '/virtualenv'
 
 def _test():
     with settings(warn_only=True):
@@ -26,27 +27,27 @@ def prepare_deploy():
 
 def _create_directory_if_necessary():
     for subfoler in ('database', 'static', 'source', 'virtualenv'):
-        run('mkdir -p %s/%s' % (project_home_directory, subfoler))
+        run('mkdir -p %s/%s' % (remote_root_directory, subfoler))
 
 def _get_latest_source():
-    if exists(project_source_directory + '.git'):
-        with cd(project_source_directory):
+    if exists(remote_git_directory + '/.git'):
+        with cd(remote_git_directory):
             run("git pull")
     else:
-        run("git clone %s %s" % (git_repo_url, project_source_directory) )
+        run("git clone %s %s" % (git_repo_url, remote_git_directory) )
         
 def _update_virtualenv():
-    if not exists(project_virtualenv_directory+'/bin/pip'):
-        run('virtualenv %s' % (project_virtualenv_directory))
-    run('%s/bin/pip install -r %s/requirements.txt' % (project_virtualenv_directory, project_source_directory))
+    if not exists(remote_virtualenv_directory+'/bin/pip'):
+        run('virtualenv %s' % (remote_virtualenv_directory))
+    run('%s/bin/pip install -r %s/requirements.txt' % (remote_virtualenv_directory, remote_source_directory))
 
 def _update_static_files():
-    with cd(project_source_directory):
-        run('%s/bin/python manage.py collectstatic --noinput' % project_virtualenv_directory )
+    with cd(remote_source_directory):
+        run('%s/bin/python manage.py collectstatic --noinput' % remote_virtualenv_directory )
 
 def _update_database():
-    with cd(project_source_directory):
-        run('%s/bin/python manage.py syncdb' % project_virtualenv_directory )
+    with cd(remote_source_directory):
+        run('%s/bin/python manage.py syncdb' % remote_virtualenv_directory )
 
 def deploy():
     _create_directory_if_necessary()
