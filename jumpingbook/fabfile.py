c76d8224a2bfd965a8cd66ee78734b70b9d32e1e
parkjuram <parkjuram@gmail.com>
2015-06-20 16:53:02 +0900

    fab file changed

--- jumpingbook/fabfile.py
+++ jumpingbook/fabfile.py
@@ -1,6 +1,9 @@
 from fabric.api import local
+from fabric.context_managers import settings
+
 
 def prepare_deploy():
     # local("./manage.py test my_app")
-    local("git add -p && git commit --allow-empty")
-    local("git push")
+    with settings(warn_only=True):
+        local("git add -p && git commit")
+        local("git push")
