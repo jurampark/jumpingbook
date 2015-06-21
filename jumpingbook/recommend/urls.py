fef04ac16ff4d809b274bfc008ff9ad56d0730ff
unknown <jjune1416@naver.com>
2015-06-21 21:09:14 +0900

    daf

--- jumpingbook/recommend/urls.py
+++ jumpingbook/recommend/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
-from recommend.views import BookListView, RatingBookView, RatedBookListView, CancelRatingBookView, SearchedBookListView
+from recommend.views import BookListView, RatingBookView, RatedBookListView, CancelRatingBookView
 
 urlpatterns = patterns('',
    url(r'^book/$', BookListView.as_view(), name='book_list'),
    url(r'^rating/book/$', RatingBookView.as_view(), name='book_rating'),
    url(r'^book/rated/$', RatedBookListView.as_view(), name='rated_book'),
    url(r'^rating/book/cancel$', CancelRatingBookView.as_view(), name='cancel_rating'),
-   url(r'^book/search/$', SearchedBookListView.as_view(), name='searched_book'),
 )
