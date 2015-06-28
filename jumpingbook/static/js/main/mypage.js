c1a4d021ae6b86fa804b193430151b671516eec4
unknown <jjune1416@naver.com>
2015-06-29 02:26:14 +0900

    adf

--- jumpingbook/static/js/main/mypage.js
+++ jumpingbook/static/js/main/mypage.js
@@ -75,9 +75,172 @@ $(document).ready(function () {
             },
             error: function (xhr, type, exception) {
                 alert("ajax error response type " + type);
+            },
+            beforeSend: function () {
+                loadingDiv.show();
+            },
+            complete: function () {
+                loadingDiv.hide();
             }
         });
     });
+
+
+    $('#check-wish-list').click(function (e) {
+        e.preventDefault();
+        mypage_container.hide();
+        check_rating_history_container.show();
+
+        $.ajax({
+            type: 'POST',
+            url: urlForGetWishList,
+            data: {},
+            success: function (data) {
+                $('input.rating').off();
+                $('.book-item-container').empty();
+                for (var index in data) {
+                    var item = data[index];
+                    var elem = "    <div class=\"book-item\" item-id=\"" + item['id'] + "\"> \
+                                    <div class=\"img-container\"> \
+                                    <img src=\"" + item['image_url'] + "\" /> \
+                                    </div> \
+                                    <div class=\"star-rating-container\"> \
+                                      <div class=\"item-title\">" + item['name'] + "</div> \
+                                      <div class=\"item-author\">" + item['author'] + "</div> \
+                                      <input type=\"hidden\" class=\"rating\" data-fractions=\"2\"/> \
+                                    </div> \
+                                    </div>";
+                    $('.book-item-container').append(elem);
+                }
+                $('input.rating').rating();
+                $('input.rating').on('change', function () {
+                    var current_item = $(this).parent().parent();
+                    $.ajax({
+                        type: 'POST',
+                        url: urlForBookRating,
+                        data: {
+                            "item-id": $(this).parent().parent().attr('item-id'),
+                            "rating": $(this).val()
+                        },
+                        success: function () {
+                            alertDiv.show();
+                            setTimeout(function() {
+                                alertDiv.hide();
+                            }, 2000);
+                            current_item.remove();
+                        },
+                        error: function (xhr, type, exception) {
+                            alert("ajax error response type2 " + type);
+                        },
+                        beforeSend: function () {
+                            loadingDiv.show();
+                        },
+                        complete: function () {
+                            loadingDiv.hide();
+                        }
+                    })
+                })
+
+
+                    $(".star-rating-container").fadeTo(0, 0);
+                    $(".book-item").mouseover(function(){
+                      //var current_item =
+                        $(this).find('div.star-rating-container').fadeTo(0, 0.8);
+                    }).mouseout(function(){
+                        $(this).find('div.star-rating-container').fadeTo(0, 0);
+                    });
+
+            },
+            error: function (xhr, type, exception) {
+                alert("ajax error response type " + type);
+            },
+            beforeSend: function () {
+                loadingDiv.show();
+            },
+            complete: function () {
+                loadingDiv.hide();
+            }
+        });
+    });
+
+    $('#check-wish-list').click(function (e) {
+        e.preventDefault();
+        mypage_container.hide();
+        check_rating_history_container.show();
+
+        $.ajax({
+            type: 'POST',
+            url: urlForGetBlackList,
+            data: {},
+            success: function (data) {
+                $('input.rating').off();
+                $('.book-item-container').empty();
+                for (var index in data) {
+                    var item = data[index];
+                    var elem = "    <div class=\"book-item\" item-id=\"" + item['id'] + "\"> \
+                                    <div class=\"img-container\"> \
+                                    <img src=\"" + item['image_url'] + "\" /> \
+                                    </div> \
+                                    <div class=\"star-rating-container\"> \
+                                      <div class=\"item-title\">" + item['name'] + "</div> \
+                                      <div class=\"item-author\">" + item['author'] + "</div> \
+                                      <input type=\"hidden\" class=\"rating\" data-fractions=\"2\"/> \
+                                    </div> \
+                                    </div>";
+                    $('.book-item-container').append(elem);
+                }
+                $('input.rating').rating();
+                $('input.rating').on('change', function () {
+                    var current_item = $(this).parent().parent();
+                    $.ajax({
+                        type: 'POST',
+                        url: urlForBookRating,
+                        data: {
+                            "item-id": $(this).parent().parent().attr('item-id'),
+                            "rating": $(this).val()
+                        },
+                        success: function () {
+                            alertDiv.show();
+                            setTimeout(function() {
+                                alertDiv.hide();
+                            }, 2000);
+                            current_item.remove();
+                        },
+                        error: function (xhr, type, exception) {
+                            alert("ajax error response type2 " + type);
+                        },
+                        beforeSend: function () {
+                            loadingDiv.show();
+                        },
+                        complete: function () {
+                            loadingDiv.hide();
+                        }
+                    })
+                })
+
+
+                    $(".star-rating-container").fadeTo(0, 0);
+                    $(".book-item").mouseover(function(){
+                      //var current_item =
+                        $(this).find('div.star-rating-container').fadeTo(0, 0.8);
+                    }).mouseout(function(){
+                        $(this).find('div.star-rating-container').fadeTo(0, 0);
+                    });
+
+            },
+            error: function (xhr, type, exception) {
+                alert("ajax error response type " + type);
+            },
+            beforeSend: function () {
+                loadingDiv.show();
+            },
+            complete: function () {
+                loadingDiv.hide();
+            }
+        });
+    });
+
+
 
     $('.mypage-list-item').click(function(e) {
         e.preventDefault();
@@ -85,5 +248,7 @@ $(document).ready(function () {
         $('.mypage-list-item.active').removeClass('active');
         $(this).addClass('active');
     });
+
+
 
 });
