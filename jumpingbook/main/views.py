94741dffba47f4b3c90f627bf6d6e34ab8904c63
parkjuram <parkjuram@gmail.com>
2015-10-08 14:53:59 +0900

    fix bug

--- jumpingbook/main/views.py
+++ jumpingbook/main/views.py
@@ -31,7 +31,7 @@ class MyPageView(LoginRequiredMixin, TemplateView):
     template_name = "users/mypage.html"
 
     def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
+        context = super(MyPageView, self).get_context_data(**kwargs)
         context['rating_book_list'] = list(UserBookRating.objects.filter(user=self.request.user).values())
         return context
 
